e40966b8bba5bc5389ca7cbb6fb97d95328f5e38
caionikolas <Caio.kanu@hotmail.com>
2022-09-02 10:32:42 -0300

    atividade03

--- atividade03/exe02.py
+++ atividade03/exe02.py
@@ -1,3 +1,3 @@
-nome = input('Digite seu nome: ').strip()
-sobrenome = input('Digite seu sobrenome: ').strip()
-print('Esse é seu nome completo ', nome, sobrenome, '!')
+nome = input().strip()
+sobrenome = input().strip()
+print(nome, sobrenome)
