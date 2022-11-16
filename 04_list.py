5e9e78b1677b0d212f930f8fc016a511b01f7b18
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2022-11-16 09:51:44 -0300

    Add files via upload

---
+++ 04_list.py
@@ -0,0 +1,30 @@
+pets = ['cao','gato','peixe']
+i = [1,'ab',[],[1,2]]
+
+# ---- Operadores ----
+pets[0]
+# 'cao'
+i[-1]
+[1,2]
+min() #retorna o menor elemento
+max() #retorna o maior elemento
+sum() #soma dos elementos
+pets[1] = 'galinha'
+pets
+# ['cao', 'galinha', 'peixe']
+
+# ---- Tuplas ----
+dias = ('seg', 'ter', 'qua')
+# iguais as listas, a diferença é que são imutavéis
+
+# ---- Métodos ----
+pets.append('cao') #adiciona um elemento no final da lista 
+count() #quantos elementos tem na lista 
+index() # retorna o elemento de uma determinada posição passad pelo parametro
+insert() # inserir um elemento numa determinada posição
+pop() # vai remove e retornar o ultimo elemento da lista
+remove() # remover o elemento da lista que é passado como parametro
+reverse() # inverter a ordem da lista
+sort() # ordenação daquela lista, seja alfabetica ou numerica
+
+
