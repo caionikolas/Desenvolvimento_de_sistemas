52ed8e1b193cc03909a1e47872142bc3e5b93392
caionikolas <Caio.kanu@hotmail.com>
2022-11-27 09:51:40 -0300

    internet

--- atividade27/exe04.py
+++ atividade27/exe04.py
@@ -17,22 +17,5 @@ def main():
 
   print(maior)
 
-  #media = 0
-  #total = 0
-  #for i in range(12):
-  #  total += jogadores[i][1]
-
-  #media = total/len(jogadores)
-
-  #maiores = []
-  #for i in range(12):
-  #  if jogadores[i][1] > media:
-  #    maiores.append(jogadores[i])
-
-  #print(f'JOGADOR MAIS ALTO DO TIME\n{maior[0]}\n{maior[1]}\nALTURA MÉDIA #DO TIME\n{media}\nJOGADORES MAIS ALTOS QUE A MÉDIA DO TIME')
-  #for i in range(len(maiores)):
-  #  print(f'{nome[0]}\n{altura[1]}')
-
-
 if __name__ == '__main__':
   main()
