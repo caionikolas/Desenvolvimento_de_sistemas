8cd19185417477c7cb6997378e15aa9b59cb6729
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2023-04-25 12:00:31 -0300

    Update exe10.py

--- Desen.Orientado_a_Testes/atv-3/exe10.py
+++ Desen.Orientado_a_Testes/atv-3/exe10.py
@@ -1,5 +1,8 @@
 def conceito(media):
-    if 
+    if (type(media) != int and type(media) != float):
+        return Exception
+    if media < 0 or media > 10:
+        return Exception
     if media >= 9:
         return "A"
     elif media >= 7:
@@ -8,3 +11,10 @@ def conceito(media):
         return "C"
     else:
         return "D"
+        
+assert conceito(8) ==  "B"   # testando classe válida
+assert conceito(2.6) == "D"    # testando classe válida
+assert conceito(16) == Exception  # testando valor improvável
+assert conceito(-4) == Exception  # testando valor improvável
+assert conceito('conceito') == Exception  # testando valor inválido
+print("Todos os testes ok!")
