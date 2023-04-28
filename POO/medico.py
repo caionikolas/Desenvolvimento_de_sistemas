a8c458c1a37fac6827165d2e000bc1944df39ed1
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2023-04-28 17:11:38 -0300

    Add files via upload

---
+++ POO/medico.py
@@ -0,0 +1,9 @@
+class Medico:
+    def __init__(self, crm, nome, especialidade):
+        self.crm = crm
+        self.nome = nome
+        self.especialidade = especialidade
+        self.id_medico = 0
+
+    def __str__(self):
+        pass
---
+++ POO/paciente.py
@@ -0,0 +1,9 @@
+class Paciente:
+    def __init__(self, nome, dt_nasc, contato):
+        self.nome = nome
+        self.dt_nasc = dt_nasc
+        self.contato = contato
+        self.id_paciente = 0
+
+    def __str__(self):
+        pass
