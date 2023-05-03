3b8460975be1f1fe5a69f580ffb94ad40ce4320c
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2023-05-03 15:22:45 -0300

    Update consulta.py

--- POO/consulta.py
+++ POO/consulta.py
@@ -13,3 +13,16 @@ class Consulta:
 maria = Paciente(32165498754, "Maria", "14/12/2002", 86998547854)
 joao = Medico(32165414521, "João", 1234, "Ortopedista")
 c1 = ConsultaMedica(1, joao, maria, "02/05/2023")
+
+
+def menu():
+    print('''
+        1 - cadastrar paciente
+        2 - cadastrar medico
+        3 - cadastrar consulta
+        4 - Pagar Consulta
+        5 - Cancelar consulta
+        6 - Agendar retorno
+        7 - Relatório de consultas realizadas no mes por médico
+        8 - Relatório de faturamento da Clinica por mes
+    ''')
