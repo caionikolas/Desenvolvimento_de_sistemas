3ace0c9435a1d3b3c49f73df3180f8ef6f8071cd
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2023-05-09 12:01:12 -0300

    Update medico.py

--- POO/medico.py
+++ POO/medico.py
@@ -1,9 +1,19 @@
-class Medico:
-    def __init__(self, crm, nome, especialidade):
-        self.crm = crm
+class Paciente:
+    def __init__(self, id, nome, dt_nasc, contato):
+        self.id_paciente = id
         self.nome = nome
-        self.especialidade = especialidade
-        self.id_medico = 0
+        self.dt_nasc = dt_nasc
+        self.contato = contato
+        
 
     def __str__(self):
         pass
+
+    def novo_paciente(self):
+        self.nome = input("Digite o nome do paciente: ")
+        self.dt_nasc = input("Digite sua data de nascimento:(d/m/aaaa) ")
+        self.contato = int(input("Digite seu telefone de contato: "))
+        id = self.gerar_codigo()
+        self.id_paciente = id
+
+maria = Paciente(32165498754, "Maria", "14/12/2002", 86998547854)
