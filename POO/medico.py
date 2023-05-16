f8168fa7dbe4b21c76f58804856355be6d4fcb78
Caio Nikolas <103952344+caionikolas@users.noreply.github.com>
2023-05-16 11:12:50 -0300

    Update medico.py

--- POO/medico.py
+++ POO/medico.py
@@ -1,18 +1,21 @@
 class Medico:
-    def __init__(self, id, nome, crm, especialidade):
-        self.id_medico = id
-        self.nome = nome
-        self.crm = crm
-        self.especialidade = especialidade
+    def __init__(self):
+        self.id_medico = 0
+        self.nome = ""
+        self.crm = 0
+        self.especialidade = ""
 
     def __str__(self):
         return f'id_medico: {self.id_paciente}, nome: {self.nome}, crm: {self.dt_nasc}, especializade: {self.contato}'
 
     def novo_medico(self):
-        self.id_medico = id
+        self.id_medico = input("Digite o id do médico: ")
         self.medico = input("digite o nome do médico: ")
-        self.crm = input("Digite seu crm ")
+        self.crm = input("Digite seu crm: ")
         self.especialidade = input("Digite sua especialidade ")
-        
 
-joao = Medico(32165414521, "João", 1234, "Ortopedista")
+        return print(f'Médico cadastrado!')
+
+#joao = Medico(32165414521, "João", 1234, "Ortopedista")
+
+
